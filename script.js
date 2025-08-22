5488a772b94fa60add0adc21b7067f49470e487c
rikuto1515 <rikuto15154242@gmail.com>
2025-08-22 11:24:55 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("addEventBtn").addEventListener("click", function() {
-    const eventList = document.getElementById("event-list");
-    const newEvent = document.createElement("li");
-    newEvent.textContent = "8月25日：来店感謝イベント - ドリンクサービス！";
-    eventList.appendChild(newEvent);
-});
-
-
